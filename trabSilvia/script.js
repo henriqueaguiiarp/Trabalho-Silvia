63130bd1e939f8177489c53fca9360e8f75851d1
henriqueaguiiarp <113645575+henriqueaguiiarp@users.noreply.github.com>
2022-10-24 20:02:35 -0300

    Trabalho HTML + Css

---
+++ trabSilvia/script.js
@@ -0,0 +1,11 @@
+function booWho(bool) {
+    if(typeof bool === true || typeof bool === false){
+        console.log(typeof bool);
+    
+    }
+    else{
+        console.log(typeof bool);
+    }
+  }
+  
+  console.log(booWho(true));
